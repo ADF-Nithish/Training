77bee3906bab1cc5de120c2335b7702e854c63ca
ADF-Nithish <nithishc@applieddatafinance.com>
2021-06-19 22:04:01 +0530

    Added: Completed Exercise-7 Rem: Flowchart and Readme.md

--- Python Exercises - 2021-06-18/Exercise-7.py
+++ Python Exercises - 2021-06-18/Exercise-7.py
@@ -1,9 +1,29 @@
 """
 Program to get an application (name , age , gender, salary, state, city)
 """
+import sys
 
 class Application(object):
-    def __init__(self,name) -> None:
-        self.__name = name
+    def __init__(self,*iterables) -> None:
+        self.__name = iterables[0]
+        self.__age = iterables[1]
+        self.__gender = iterables[2]
+        self.__salary = iterables[3]
+        self.__state = iterables[4]
+        self.__city = iterables[5]
+    
+    def __getvalue(self):
+        return " , ".join(["Name: "+str(self.__name),"Age: "+str(self.__age), "Gender: "+str(self.__gender), "Salary: "+str(self.__salary), "State: "+str(self.__state), "City: "+str(self.__city)])
+    def __repr__(self) -> str:
+        return self.__class__.__name__+"("+self.__getvalue()+")"
+    
 
-app = Application('Nithish')
+app = Application(*sys.argv[1:])
+print(app)
+
+"""
+Command Line Arguement:
+    py Exercise-7.py <inputs>
+Eg:
+    py Exercise-7.py Nithish 20 Male 18000 Tamilnadu Coimbatore
+"""
